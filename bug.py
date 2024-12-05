84ecd4b7189dbe6eb802fbbf37e7b0a665ebec9b
BugHunter-X <askzenai@gmail.com>
2024-12-05 23:00:05 +0100

    Add bug.py file

---
+++ bug.py
@@ -0,0 +1,10 @@
+def function_with_uncommon_error(a, b):
+    if a == 0:
+        return b
+    elif b == 0:
+        return a
+    else:
+        return a / b
+
+result = function_with_uncommon_error(0, 0)
+print(result) #This will throw an error because it is not defined in the return statment for a==0 and b==0 case
